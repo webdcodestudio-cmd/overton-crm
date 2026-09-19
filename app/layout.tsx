39b0356d4ffdac708cd6b7ed2da4948e933d3996
webdcodestudio <web.dcodestudio@gmail.com>
2026-09-19 17:42:52 +0530

    Add Meta Pixel tracking code (ID 1019021857847335) to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -169,10 +169,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-IN" className="scroll-smooth">
+      <head>
+        {/* ── META PIXEL ── */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1019021857847335');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body
         className={`${bricolageGrotesque.variable} ${instrumentSans.variable} antialiased`}
         suppressHydrationWarning
       >
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1019021857847335&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ModalProvider>{children}</ModalProvider>
         {/* ── GOOGLE ANALYTICS ── */}
         <Script
